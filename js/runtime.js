f21be511ea6d9c721a449d57b2fe0def966a0321
dongliangrui <2366869238@qq.com>
2022-12-03 21:25:08 +0800

    Site updated: 2022-12-03 21:25:08

--- js/runtime.js
+++ js/runtime.js
@@ -1,35 +1,33 @@
-setInterval(() => {
-    let create_time = Math.round(new Date('2022-12-1 00:00:00').getTime() / 1000); //在此行修改建站时间
-    let timestamp = Math.round((new Date().getTime()) / 1000);
-    let second = timestamp - create_time;
-    let time = new Array(0, 0, 0, 0, 0);
+var now = new Date;
 
-    var nol = function (h) {
-        return h > 9 ? h : '0' + h;
-    }
-    if (second >= 365 * 24 * 3600) {
-        time[0] = parseInt(second / (365 * 24 * 3600));
-        second %= 365 * 24 * 3600;
-    }
-    if (second >= 24 * 3600) {
-        time[1] = parseInt(second / (24 * 3600));
-        second %= 24 * 3600;
-    }
-    if (second >= 3600) {
-        time[2] = nol(parseInt(second / 3600));
-        second %= 3600;
-    }
-    if (second >= 60) {
-        time[3] = nol(parseInt(second / 60));
-        second %= 60;
-    }
-    if (second > 0) {
-        time[4] = nol(second);
-    }
-    if ((Number(time[2]) < 22) && (Number(time[2]) > 7)) {
-        currentTimeHtml = "<img class='boardsign' src='https://img.shields.io/badge/糖果屋-营业中-6adea8?style=social&logo=cakephp' title='距离百年老店也就差不到一百年~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
-    } else {
-        currentTimeHtml = "<img class='boardsign' src='https://img.shields.io/badge/糖果屋-打烊了-6adea8?style=social&logo=coffeescript' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
-    }
-    document.getElementById("workboard").innerHTML = currentTimeHtml;
-}, 1000);
+function createtime() {
+    now.setTime(now.getTime() + 1e3);
+    var o = new Date("12/01/2022 00:00:00"),
+        r = (now - o) / 1e3 / 60 / 60 / 24,
+        i = Math.floor(r),
+        n = (now - o) / 1e3 / 60 / 60 - 24 * i,
+        s = Math.floor(n);
+    1 == String(s).length && (s = "0" + s);
+    var l = (now - o) / 1e3 / 60 - 1440 * i - 60 * s,
+        g = Math.floor(l);
+    1 == String(g).length && (g = "0" + g);
+    var d = (now - o) / 1e3 - 86400 * i - 3600 * s - 60 * g,
+        b = Math.round(d);
+    1 == String(b).length && (b = "0" + b);
+    let c = "";
+    c = s < 18 && s >= 9 ? 
+    `<img class='boardsign' src='https://img.shields.io/badge/D%E5%B0%8F%E5%B1%8B-%E4%B8%8A%E7%8F%AD%E6%91%B8%E9%B1%BC%E4%B8%AD-brightgreen' 
+    title='距离月入25k也就还差一个大佬带我~'>
+    <br> 本站已经运行了 ${i} 天 ${s} 小时 ${g} 分 ${b} 秒 
+    <i id="heartbeat" class='fas fa-heartbeat'></i> 
+    <br> 看看那个光点，它就在这里，这是家园，这是我们 —— 你所爱的每一个人，你认识的一个人，你听说过的每一个人，曾经有过的每一个人，都在它上面度过他们的一生✨ ` : 
+    `<img class='boardsign' src='https://img.shields.io/badge/D%E5%B0%8F%E5%B1%8B-%E6%89%93%E7%83%8A%E4%BC%91%E6%81%AF%E5%95%A6-blue' 
+    title='下班了就该开开心心的玩耍，嘿嘿~'>
+    <br> 本站居已经运行了 ${i} 天 ${s} 小时 ${g} 分 ${b} 秒 
+    <i id="heartbeat" class='fas fa-heartbeat'></i> 
+    <br> 看看那个光点，它就在这里，这是家园，这是我们 —— 你所爱的每一个人，你认识的一个人，你听说过的每一个人，曾经有过的每一个人，都在它上面度过他们的一生✨ `, 
+    document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = c)
+}
+setInterval((() => {
+    createtime()
+}), 1e3);
